04f86f9b034885509c113a38b0ea1b8de299bd33
Matheus Rodrigues <matheus.rodrigues_martins@hotmail.com>
2023-10-01 12:29:09 -0300

    feat: Yup translat and multiple error validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,14 +1,38 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+//import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
-interface ICidade{
+
+interface ICidade {
   nome: string;
+  estado: string;
 }
 
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
+});
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
+  try {
+    validatedData = await bodyValidation.validate(req.body, { abortEarly: false});
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+  
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
 
-  console.log(req.body);
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: errors,
+    });
+  }
 
+  console.log(validatedData);
 
   return res.send('Create!!');
 };
