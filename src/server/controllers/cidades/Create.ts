23a788cac382d2d35dd14387f575b5b91103ed03
Matheus Rodrigues <matheus.rodrigues_martins@hotmail.com>
2023-10-05 22:00:32 -0300

    feat: implement get all

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -8,28 +8,16 @@ import { validation } from '../../shared/middlewares';
 
 interface ICidade {
   nome: string;
-  estado: string;
 }
-
-interface IFilter {
-  filter?: string;
-  //limit?: number;
-}
-
-
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICidade>(yup.object().shape({
     nome: yup.string().required().min(3),
-    estado: yup.string().required().min(3),
-  })),
-  query: getSchema<IFilter>(yup.object().shape({
-    filter: yup.string().required().min(3),
   })),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   console.log(req.body);
 
-  return res.send('Create!!');
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não Implementado!!');
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -10,9 +10,8 @@ router.get('/', (_, res) => {
   return res.send('teste resposta');
 });
 
-router.post('/cidades',
-  CidadesController.createValidation,
-  CidadesController.create);
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
 
 
 
